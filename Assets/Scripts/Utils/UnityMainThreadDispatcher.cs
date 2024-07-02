3a339eecc116e250cd5066945600a8075e1ce0a2
Nikki <nikki.asteinza@gmail.com>
2024-07-02 13:42:14 +0200

    Update UnityMainThreadDispatcher.cs

--- Assets/Scripts/Utils/UnityMainThreadDispatcher.cs
+++ Assets/Scripts/Utils/UnityMainThreadDispatcher.cs
@@ -1,3 +1,19 @@
+// Author: Nikki Asteinza (2024-06-22)
+
+//This Unity script is designed to execute actions on Unity's main thread from other threads.
+//This is particularly useful for operations that need to interact with Unity's API, which must be called from the main thread.
+
+//Functionality:
+//Singleton Pattern: Ensures only one instance of UnityMainThreadDispatcher exists throughout the application's lifecycle.
+//Action Queue: Maintains a queue of actions to be executed on the main thread.
+//Thread-Safe Enqueueing: Allows other threads to enqueue actions safely for execution on the main thread.
+//Main Thread Execution: Executes all queued actions on the main thread in the Update method.
+
+//Key Features:
+//Singleton Instance: Ensures a single instance using DontDestroyOnLoad to persist across scene loads.
+//Thread Safety: Uses a lock mechanism to ensure that actions are enqueued and dequeued safely across multiple threads.
+//Main Thread Execution: Dequeues and executes actions in the Update method, ensuring they run on the main thread.
+
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
